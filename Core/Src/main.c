4e2aeb35d4a09510cc1686875425cf71ce7fbc25
EmmanuelDuguin <emmanuel.duguin@gmail.com>
2022-12-08 12:02:37 +0100

    TP2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -103,10 +103,6 @@ int main(void)
 	HAL_UART_Receive_IT(&huart2, uartRxBuffer, UART_RX_BUFFER_SIZE);
 	HAL_Delay(1);
 	shellInit();
-	HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_1);
-	HAL_TIMEx_PWMN_Start(&htim1,TIM_CHANNEL_1);
-	HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
-	HAL_TIMEx_PWMN_Start(&htim1,TIM_CHANNEL_2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
